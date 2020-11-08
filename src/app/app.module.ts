d78d4b566fdd05b432c078722ff115ff5dcb398d
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-08 22:20:27 +0200

    imported some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteComponent } from './quotes/quotes.component';
+import { QuoteFormComponent } from './quotes-form/quotes-form.component';
+import { AppRoutingModule } from './app-routing.module';
 import { DateCountPipe } from './date-count.pipe';
-import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
-    DateCountPipe,
-    QuotesFormComponent
+    QuoteComponent,
+    QuoteFormComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
