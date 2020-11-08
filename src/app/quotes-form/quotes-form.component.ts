c8dc5cce3e11c4799577180d27d8e8f38773c477
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-08 22:25:18 +0200

    adding function to submit quotes

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import{Quotes} from '../quotes'
 
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
   styleUrls: ['./quotes-form.component.css']
 })
-export class QuotesFormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
+  newQuote = new Quotes(0,'','','',0,0,'',new Date());
 
+  @Output() addQuote= new EventEmitter<Quotes>();
+  
+     submitQuote(){
+      this.addQuote.emit(this.newQuote);
+    }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
