a2f004abdaa9b6b29a64d219c9ae7fa53785427f
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-08 22:22:08 +0200

    added  constuctor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,12 @@
 export class Quotes {
+    constructor(
+        public id:number,
+        public author: string,
+        public title: string,
+        public quote: string,
+        public upvotes: number,
+        public downvotes: number,
+        public submitedBy: string,
+        public date:Date
+         ){}
 }
